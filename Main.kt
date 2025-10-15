704354df89f70f7bb709428d345a333a4b8947e2
injilmi <167489251+janiolangel@users.noreply.github.com>
2025-10-15 20:30:06 +0800

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -2,7 +2,7 @@
 // provides REPL (Read-Eval-Print Loop)
 
 fun main() {
-    println("Lexical cmsc124_lab1.Scanner (type 'exit' to quit)")
+    println("Lexical cmsc124_lab2.Parser (type 'exit' to quit)")
     val input = java.util.Scanner(System.`in`)
 
     while (true) {
@@ -11,12 +11,18 @@ fun main() {
         val line = input.nextLine()
         if (line.trim() == "exit") break
 
-        // send and return
+        // Scanner (send and return)
         val scanner = Scanner(line)
         val tokens = scanner.scanTokens()
 
-        // join tokens with newlines
-        println(tokens.joinToString("\n"))
+        // Parser
+        val parser = Parser(tokens)
+        val expression = parser.parse()
+
+        // Print AST if parsing succeeded
+        if (expression != null) {
+            val printer = AstPrinter()
+            println(printer.print(expression))
+        }
     }
 }
-
