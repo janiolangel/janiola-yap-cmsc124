8ca52e2789a6f4afdb550161e20fa09b8dc1ce24
MM Yap <mryap2@up.edu.ph>
2025-11-05 14:01:38 +0800

    Delete Keywords.kt

--- Keywords.kt
+++
@@ -1,9 +0,0 @@
-// Keywords map for identifiers
-val keywords = mapOf(
-    "var" to TokenType.VAR,
-    "if" to TokenType.IF,
-    "else" to TokenType.ELSE,
-    "while" to TokenType.WHILE,
-    "for" to TokenType.FOR,
-    "return" to TokenType.RETURN
-)
