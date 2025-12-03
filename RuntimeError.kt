62858e71e7e496dedf3799794343217e1834611d
MM Yap <mryap2@up.edu.ph>
2025-12-03 21:33:24 +0800

    added new functions to meet lab 5 requirements

--- RuntimeError.kt
+++ RuntimeError.kt
@@ -1,3 +1,1 @@
-// REPORT CLEAR, HELPFUL ERROR MESSAGES THAT INCLUDE LINE NUMBERS
-// TO BE PRINTED IN MAIN.KT
 class RuntimeError(val token: Token, message: String) : RuntimeException(message)
--- Token.kt
+++ Token.kt
@@ -1,18 +1,28 @@
+/*
+ * LAB 5
+ * Functions
+ */
+
 enum class TokenType {
     // Structure
     LEFT_PAREN, RIGHT_PAREN,
     LEFT_BRACE, RIGHT_BRACE,
-    SEMICOLON,
+    SEMICOLON, COMMA,
 
     // Gen Z verbs (expressions)
     MIX, TAKE_AWAY, MULTIPLY, DIVIDE, FLIP, CHECK_IF,
 
-    // Statement keywords
+    // Statement keywords (existing + new cookbook)
     REMEMBER, SET, PRINT, AS, TO,
     THE, VALUE, WHEN, YOU,
 
+    // New cookbook-style control flow keywords
+    IF, ELSE,           // mapped from "when" / "otherwise"
+    WHILE, FOR,         // "repeat" / "for"
+    FUN, RETURN,        // "recipe" / "serve"
+
     // Connectors for expressions
-    AND, FROM, WITH,
+    AND, FROM, WITH, OR,
 
     // Comparison
     GREATER, LESS, EQUAL_EQUAL,
